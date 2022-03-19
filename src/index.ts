155f928df9c1a7adcef003bec4304fb494e2754a
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2022-03-19 21:21:32 +0700

    fix: fix preview and breakpoints from sm to md

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import "./styles/index.css";
-import Swiper, { Autoplay } from "swiper";
+import Swiper, { Autoplay, Pagination } from "swiper";
 import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
 
-Swiper.use([Autoplay]);
-const swiper = new Swiper(".swiper", {
+Swiper.use([Autoplay, Pagination]);
+const heroSwiper = new Swiper(".hero", {
   loop: true,
+  speed: 500,
   autoplay: {
-    delay: 5000,
+    delay: 3000,
+    disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
-    type: "bullets",
+  },
+});
+
+const previewSwiper = new Swiper(".person", {
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
   },
 });
