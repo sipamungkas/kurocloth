1d2c08b3f95ddf77b021d788ac226990b70b9b8d
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2022-03-23 18:48:11 +0700

    feat: add send to whatsapp message

--- src/index.ts
+++ src/index.ts
@@ -43,3 +43,16 @@ imagePersons.forEach((imagePerson) => {
 function imagePersonListener(this: HTMLImageElement, ev: Event) {
   imageDisplay.src = this.src;
 }
+
+const waButton = document.querySelector("#wa-button")!;
+
+waButton.addEventListener("click", sendWAMessage);
+
+function sendWAMessage(this: Element, ev: Event) {
+  const encodedMessage =
+    "Halo%20Kak.%20Saya%20mau%20pesan%20kaos%20THIRD%20nya.%0A%0ADengan%20keterangan%20%3A%0ANama%20pemesan%20%3A%0AAlamat%20penerima%20%3A%0ANo.%20WhatsApp%20penerima%20%3A%0AJumlah%20%3A%0AUkuran%20%3A%0A%0ATerimakasih%20Banyak%20kak";
+  const number = "+6285156119394";
+  return window.open(
+    `https://api.whatsapp.com/send?phone=${number}&text=${encodedMessage}`
+  );
+}
